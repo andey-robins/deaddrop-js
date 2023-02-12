9fc4fd659194ae108c5e45f75ab7e28215e1925a
Derek Walton <90069173+Dewed02@users.noreply.github.com>
2023-02-11 21:52:00 -0700

    add array to log user and read time
    also write out to a file

--- src/read.ts
+++ src/read.ts
@@ -13,8 +13,21 @@ export async function readMessages(user: string) {
 
         getMessagesForUser(user).then((messages) => {
             messages.forEach((e: string) => console.log(e, "\n"));
-            
-        userReadMessage(user);
+        var userRead = [];  //Desperation using array to log user info
+        var date = new Date(); //Date object to log access time
+        let time = alert(date);
+        userRead.push(user);
+        let timeAndDate = time.toString(); //Convert time to string 
+        userRead.push(timeAndDate);
+        // Taken from https://stackoverflow.com/questions/17614123/node-js-how-to-write-an-array-to-file  
+        const fs = require('fs');
+        var file = fs.createWriteStream('array.txt');
+        file.on('error', function(err) { /* error handling */});
+        userRead.forEach(function(v) { file.write(v.join(', ') + '\n'); })
+        file.end();
+         
+        
+        userReadMessage(user);  //Attempt at using database to store user will try to figure out later 
         });
 
     } catch (error) {
