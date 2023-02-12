86dd1d579bc6c30c243435ecd1971d1db0c0c153
Derek Walton <90069173+Dewed02@users.noreply.github.com>
2023-02-12 14:41:18 -0700

    Update send.ts

--- src/send.ts
+++ src/send.ts
@@ -1,10 +1,15 @@
 import readline from "readline";
 import { saveMessage, userExists } from "./db";
+import { authenticate } from "./session"; // Used to make users sign in before sending a message
 
 export const sendMessage = async (user: string) => {
     try {
         if (!await userExists(user)) {
             throw new Error("Destination user does not exist");
+        }
+        
+        if (!await authenticate(user)) {
+            throw new Error("Unable to authenticate"); //Requires user to sign in before sending message
         }
 
         getUserMessage().then(async (message) => {
