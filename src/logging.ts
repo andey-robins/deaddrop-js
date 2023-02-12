c800b7059c034999517aaf919f33cbfd571cc5f9
Derek Walton <90069173+Dewed02@users.noreply.github.com>
2023-02-11 21:05:12 -0700

    Create method to store user

---
+++ src/logging.ts
@@ -0,0 +1,16 @@
+import { connect } from "/.db"
+
+export const userReadMessage = async (reader: string) => {
+    let db = await connect();
+   
+    await db.run(`
+        INSERT INTO MessageRead
+            (reader)
+        VALUES (
+            (SELECT id FROM Users WHERE user = :user),
+            :reader
+        )
+`, {
+    ":user": reader, 
+   });
+}
