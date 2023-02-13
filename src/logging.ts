4ae75ed12143fa655f5e6001afd9b96c92293a30
Derek Walton <90069173+Dewed02@users.noreply.github.com>
2023-02-13 14:13:10 -0700

    Update logging.ts

--- src/logging.ts
+++ src/logging.ts
@@ -1,11 +1,11 @@
 import { connect } from "./db/db"
 
-export const userReadMessage = async (user: string) => {
+export const userReadMessage = async (username: string) => {
     let db = await connect();
    
     await db.run(`
         INSERT INTO MessageRead
-            (user)
+            (username)
         VALUES (
             (SELECT user FROM Users WHERE user = :user)
         )
