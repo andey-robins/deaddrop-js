9f3113921bdf257d911c611acb17a5d22240999e
Derek Walton <90069173+Dewed02@users.noreply.github.com>
2023-02-13 17:46:39 -0700

    Update logging.ts

--- src/logging.ts
+++ src/logging.ts
@@ -13,3 +13,17 @@ export const userReadMessage = async (username: string) => {
     ":user": username,
    });
 }
+
+export const userSentMessage = async (username: string) => {
+   let db = await connect();
+    
+    await db.run(`
+    INSERT INTO MessageSent
+            (username)
+        VALUES (
+            (SELECT user FROM Users WHERE user = :user)
+        )
+`, {
+    ":user": username,
+   });
+}
